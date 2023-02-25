16318a2bde842d1309a6e739aecffd16d76b15dd
shangkyu.shin <sskyu2@naver.com>
2023-02-25 16:34:38 +0900

    test

--- run.py
+++ run.py
@@ -15,5 +15,5 @@ except KeyError:
 app = create_app(config_mode)
 Migrate(app, db)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000) 
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000) 
